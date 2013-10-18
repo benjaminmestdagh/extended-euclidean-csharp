224407dd7ab83b530a0708c7160e4ec3062839ef
Benjamin Mestdagh <benjaminmestdagh@gmail.com>
2013-10-18 12:38:15 +0200

    Initial commit

---
+++ EuclidExtended/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace EuclidExtended
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblA = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.lblB = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(33, 28);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(16, 13);
+            this.lblA.TabIndex = 0;
+            this.lblA.Text = "a:";
+            this.lblA.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(55, 25);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 1;
+            this.txtA.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtA_KeyDown);
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(55, 63);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 3;
+            this.txtB.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtB_KeyDown);
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(33, 66);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(16, 13);
+            this.lblB.TabIndex = 2;
+            this.lblB.Text = "b:";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(36, 108);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(119, 23);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(241, 27);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(193, 104);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.Text = "ax + by = 1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 166);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.lblA);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Euclid\'s Extended Algorithm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ EuclidExtended/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EuclidExtended
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            performCalculation();
+        }
+
+        private void txtA_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Return)
+            {
+                txtB.Focus();
+            }
+        }
+
+        private void txtB_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Return)
+            {
+                performCalculation();
+            }
+        }
+
+        private void performCalculation()
+        {
+            try
+            {
+                long a = long.Parse(txtA.Text);
+                long b = long.Parse(txtB.Text);
+
+                EuclidExtended ee = new EuclidExtended(a, b);
+                EuclidExtendedSolution result = ee.calculate();
+
+                lblResult.Text = string.Format("d = {0} {1}x = {2} {1}y = {3}", result.D, Environment.NewLine, result.X, result.Y);
+            }
+            catch (Exception ex)
+            {
+                String message = string.Format("That doesn't seem right:{0}\"{1}\"", Environment.NewLine, ex.Message);
+                MessageBox.Show(message, "Whoopsie!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
